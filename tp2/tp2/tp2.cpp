8efab41ef1c5075f5550994427ea79f37c68a995
Guillaume Hostache <guillaume.hostache_pro@outlook.fr>
2023-10-26 15:25:36 +0200

    ajout seance 4 tp2

--- tp2/tp2/tp2.cpp
+++ tp2/tp2/tp2.cpp
@@ -10,6 +10,12 @@
 #include <string>
 #include <time.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+
+#include "stb_image.h"
+#include "stb_image_write.h"
+
 // Taille des images que l'on génère pour le début de l'exercice 2
 const int TAILLE = 255;
 
@@ -118,6 +124,46 @@ void exo2_p4() {
     img_3.close();
 }   
 
+// manipulation d'images
+void exo2_p5() {
+    int width, height, channels;
+    unsigned char* img = stbi_load("lena.png", &width, &height, &channels, 0);
+
+    std::cout << "Dimension de l'image : (" << width << ", " << height << ")" << std::endl;
+    std::cout << "Nombres de cannaux : " << channels << std::endl;
+    
+    stbi_write_png("lena_a.png", width, height, channels, (void*)img, 0);
+    stbi_image_free(img);
+
+    unsigned char* lena_b = stbi_load("lena.png", &width, &height, &channels, 0);
+    for (int i = height / 3; i < 2 * (height / 3); i++) {
+        for (int j = width / 3; j < 2 * (width / 3); j++) {
+            int ipixel = (i * width + j) * channels;
+            lena_b[ipixel] = 0;
+            lena_b[ipixel + 1] = 0;
+            lena_b[ipixel + 2] = 0;
+        }
+    }
+    stbi_write_png("lena_b.png", width, height, channels, (void*)lena_b, 0);
+    stbi_image_free(lena_b);
+
+    unsigned char* lena_c = stbi_load("lena.png", &width, &height, &channels, 0);
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            int ipixel = (i * width + j) * channels;
+            int moyenne = (lena_c[ipixel] + lena_c[ipixel + 1] + lena_c[ipixel + 2] + lena_c[ipixel + 3]) / 4;
+            if (100 < moyenne && moyenne < 150) {
+                lena_c[ipixel] = 255;
+                lena_c[ipixel + 1] = 255;
+                lena_c[ipixel + 2] = 255;
+                lena_c[ipixel + 3] = 255;
+            }
+        }
+    }
+    stbi_write_png("lena_c.png", width, height, channels, (void*)lena_c, 0);
+    stbi_image_free(lena_c);
+}
+
 int main()
 {
     std::cout << "Exercice (entree/sortie fichier) :" << std::endl;
@@ -125,10 +171,10 @@ int main()
     //exo2_p2();
     //exo2_p3();
     std::cout << "\nExercice (generations d'images) :" << std::endl;
-    exo2_p4();
+    //exo2_p4();
     std::cout << "\nExercice (manipulations d'images) :" << std::endl;
-    // à venir
+    exo2_p5(); // fini
     std::cout << "\nExercice (filtres d'images) :" << std::endl;
-    // à venir
+    // bonus
     return EXIT_SUCCESS;
 }
